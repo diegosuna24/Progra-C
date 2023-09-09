23cf6a271053a5ef50347658540fdc6bcd2d3f72
diegosuna24 <osunaacevedo24@gmail.com>
2023-09-08 21:03:50 -0700

    taller22

--- Practica2_Tipos_Operadores_y_Expreciones_Osuna_Acevedo_Diego/Problema3.cpp
+++ Practica2_Tipos_Operadores_y_Expreciones_Osuna_Acevedo_Diego/Problema3.cpp
@@ -5,6 +5,7 @@
 //TAller2_actividad3
 #include<stdio.h>
 
+//declarar el arreglo
 int almacen [10];
 int cont, espacio, cantidad, operacion;
 
@@ -16,22 +17,28 @@ int main(){
         scanf("%d",&operacion);
          printf("que cantidad? ");
         scanf("%d",&cantidad);
+        //elegimos si queremos agregar o quitar 
         if(operacion==1){
+            //Nos aseguramos que exista suficente espacio
             if(almacen[espacio]+cantidad>10){
             printf("Este espacio esta lleno\n");
         }
         else{
+            //Agregamos la cantidad al arreglo
             almacen[espacio]=almacen[espacio]+cantidad;
         }
         }
         else{
+            //nos aseguremos que tengamos suficientes cosas en el espacio
             if(almacen[espacio]-cantidad<0){
                 printf("No es posible quitar tanto\n");
             }
             else{
+                //Eliminamo la cantidad deseada
                 almacen[espacio]=almacen[espacio]-cantidad;
             }
         }
+        //Preguntamos si continuar o no
         printf("Quieres continuar?\n si==0 \n no==1");
         cont=scanf("%d",&cont);
 
--- Practica2_Tipos_Operadores_y_Expreciones_Osuna_Acevedo_Diego/Problema_2.cpp
+++ Practica2_Tipos_Operadores_y_Expreciones_Osuna_Acevedo_Diego/Problema_2.cpp
@@ -9,17 +9,21 @@ int cal;
 
 int main(){
  printf("Da tu calificacion del 0 al 100;  ");
-        scanf("%d",&cal);
+        scanf("%d",&cal);                            //Pedir calificacion
 
         
-        if(cal>=90){
-            printf("Tu calificacion es:  A"); }
+        if(cal>=90&&cal<=100){
+            printf("Tu calificacion es:  A"); }   // Si la calificacion de encuantra entre 90 y 80 la calificacion es B
          if(cal<90 && cal>=80){
             printf("Tu calificacion es:  B");}
          if(cal<80 && cal>=79){
             printf("Tu calificacion es:  C");}
-         if(cal<70 && cal>=69){
+         if(cal<70 && cal>=60){
             printf("Tu calificacion es:  D");}
-         if(cal<60){
+         if(cal<60&&cal>=0){
             printf("Tu calificacion es:  F");}
+            else
+            {
+               printf("Error");                       //si la calificacion no se encuantra en el rango de 0 a 100 hay un error
+            }
 }
